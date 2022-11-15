8b0f185616353b7504a22ce12ff096aff37c5c65
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-11-15 15:47:11 +0600

    Step 1: model Product and used migrations

--- bishop/webapp/admin.py
+++ bishop/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import Product
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'details', 'status', 'balance', 'price']
+    list_display_links = ['product']
+    list_filter = ['status']
+    search_fields = ['product']
+    exclude = []
+
+admin.site.register(Product, ProductAdmin)
+
 
 # Register your models here.
--- bishop/webapp/models.py
+++ bishop/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+STATUS_CHOICES = [('other', 'Other'), ('electronics', 'Electronics'), ('garden', 'Garden'), ('food', 'Food')]
 
-# Create your models here.
+class Product(models.Model):
+    product = models.CharField(max_length=100, null=False, blank=False, verbose_name="Наименование товара")
+    details = models.TextField(max_length=2000, null=True, blank=True, default="-", verbose_name="Описание товара")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name="Статус")
+    balance = models.PositiveIntegerField(blank=True, null=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+
+    def __str__(self):
+        return f'{self.id}. {self.product}'
