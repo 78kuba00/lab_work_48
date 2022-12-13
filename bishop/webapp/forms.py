68feb434eb1833819f3b36a38d780553b109487e
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-12-13 16:56:17 +0600

    trying to make CRUD

--- bishop/webapp/forms.py
+++ bishop/webapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import widgets
-from webapp.models import CATEGORIES_CHOICES
+from webapp.models import CATEGORIES_CHOICES, Product
 
 
 class SearchForm(forms.Form):
@@ -8,8 +8,12 @@ class SearchForm(forms.Form):
 
 
 class ProductForm(forms.Form):
-    name = forms.CharField(max_length=100, required=True, label='Наименование товара')
-    description = forms.CharField(max_length=2000, required=False, label='Описание товара', widget=widgets.Textarea)
-    category = forms.ChoiceField(choices=CATEGORIES_CHOICES, label='Категория')
-    balance = forms.IntegerField(min_value=0, required=True, label='Остаток')
-    price = forms.DecimalField(max_digits=7, decimal_places=2, label='Стоимость')
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'category', 'balance', 'price']
+        exclude = ['balance']
+    # name = forms.CharField(max_length=100, required=True, label='Наименование товара')
+    # description = forms.CharField(max_length=2000, required=False, label='Описание товара', widget=widgets.Textarea)
+    # category = forms.ChoiceField(choices=CATEGORIES_CHOICES, label='Категория')
+    # balance = forms.IntegerField(min_value=0, required=True, label='Остаток')
+    # price = forms.DecimalField(max_digits=7, decimal_places=2, label='Стоимость')
